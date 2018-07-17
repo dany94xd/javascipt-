13290e2b3db3de86b4fdff657d106edffd912df4
Hector-Luis <hluischimbo@gmail.com>
2018-07-17 14:06:36 -0500

    videos youtube

--- js/jsvideo.js
+++ js/jsvideo.js
@@ -7,8 +7,25 @@
 window.onload = function(){
   var urlVideo = window.opener.document.getElementById("urlVideo").innerHTML;
   var indexVideo = window.opener.document.getElementById("indexVideo").innerHTML;
+  var valor = urlVideo.indexOf("https");
+  //alert(valor);
+  var cadena = "";
+  if (valor == -1) {
+    //alert("es local");
+    cadena = "<iframe src='" + urlVideo + "' frameborder='0' allowfullscreen></iframe>";
+  }else{
+    //alert("hola");
+    var patron = "https://www.youtube.com/watch?v=";
+    var nuevoValor = "https://www.youtube.com/embed/";
+    urlVideo = urlVideo.replace(patron, nuevoValor);
+    //alert(urlVideo);
+    cadena = "<iframe src='" + urlVideo + "' frameborder='0' allowfullscreen></iframe>";
+  }
+
+  /**/
   //window.opener.close();
-  var cadena = "<source src='" + urlVideo + "' type='video/mp4'>";
+  //var cadena = "<source src='" + urlVideo + "' type='video/mp4'>";
+  //var cadena = "<iframe src='https://www.youtube.com/embed/" + urlVideo + "' frameborder='0' allowfullscreen></iframe>";
   document.getElementById("video").innerHTML=cadena;
   //alert(urlVideo);
    
